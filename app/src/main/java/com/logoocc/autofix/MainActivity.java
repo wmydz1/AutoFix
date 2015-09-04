4ae3ec05f3d076f74d4cb3b0146710e4a195b529
samchen <git@samchen.com>
2015-09-04 17:55:59 +0800

    百分比布局
    百分比布局

---
+++ app/src/main/java/com/logoocc/autofix/MainActivity.java
@@ -0,0 +1,15 @@
+package com.logoocc.autofix;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_fix);
+    }
+
+
+}
